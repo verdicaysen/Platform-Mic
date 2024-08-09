d698d08133d5ae89fefe761a9bbd10e3f3d59205
rashley <verdicaysen@gmail.com>
2024-08-08 23:17:33 -0500

    Transitioned everything to C# and added Trash Can for when the player falls off the platform. It will send them back to the start position node.

--- scripts/Level.cs
+++ scripts/Level.cs
@@ -4,16 +4,32 @@ using System;
 public partial class Level : Node2D
 
 {
+    private Marker2D myStartPosition;
+
     public override void _Ready()
     {
-        
+        //Welcome to how to connect Signals in C# 101. You're not stupid anymore!
+        TrashCan myTrashCan = GetNode<TrashCan>("TrashCan");
+        //Yes, the += is necessary. We're saying hey node we just referenced, use the signal we connected and then add it to the method OnTrashCanBodyEntered.
+        myTrashCan.BodyEntered += OnTrashCanBodyEntered;
+        myStartPosition= GetNode<Marker2D>("StartPosition");
     }
 
-    public override void _PhysicsProcess(double delta)
-	{
+    //Here we're using the signal with the method we already previously declared to the signal handler.
+     private void OnTrashCanBodyEntered(Node2D body)
+    {
+        // If the body is a CharacterBody2D, reset its velocity and position as directed.
+        if (body is CharacterBody2D characterBody)
+        {
+            // Reset the velocity to zero.
+            characterBody.Velocity = Vector2.Zero;
 
+            // Reset the characters position to these coordinates.
+            characterBody.GlobalPosition = myStartPosition.GlobalPosition;
+        }
     }
 
 }
 
 
+ 
